934bce71130d873a3a60361049fa89d34f8fb98e
Derpy666 <37512753+Derpy666@users.noreply.github.com>
2018-03-23 17:39:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,56 +2,56 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  client.user.setStatus("online");
-  client.user.setGame("/help | Derpy Bot");
+  client.user.setStatus("dnd");
+  client.user.setGame("/network help | MigServer Bot");
   console.log("Reload Completed!");
 });
 
 client.on('message', msg => {
-  if (msg.content === `/test`) {
+  if (msg.content === `/network test`) {
     msg.reply('I am Online');
   }
 });
 
 client.on('message', msg => {
-    if (msg.content === `/discord`) {
-      msg.channel.sendMessage('**__Discord Link:__**\n \nhttps://discord.gg/8Z2gENf');
+    if (msg.content === `/network discord`) {
+      msg.channel.sendMessage('**__Discord Link:__**\n \nhttps://discord.gg/JuuVp5D');
     }
   });
 
   client.on('message', msg => {
-    if (msg.content === `/apply`) {
-      msg.channel.sendMessage('**__Staff Apply:__**\n \nhttps://docs.google.com/forms/d/e/1FAIpQLSecQQcz2-MfH1Aenr5xDr_2ekqDkD-km2nT54iR1ocV_BWj7w/viewform');
+    if (msg.content === `/network apply`) {
+      msg.channel.sendMessage('**__Staff Apply:__**\n \nhttps://docs.google.com/forms/d/e/1FAIpQLSdAcbc6iCUbI_YnKiMAKwXVxl87nuDJFb5WbA_Mutthrw4BDw/viewform');
     }
   });
 
   client.on('message', msg => {
-    if (msg.content === `/creator`) {
-      msg.channel.sendMessage('The Creator of the Bot is `Derpy#6522` and `[PROGRAMMER] KOSTA#3066`');
+    if (msg.content === `/network creator`) {
+      msg.channel.sendMessage('The Creator of the Bot is `Derpy#6522`');
     }
   });
 
   client.on('message', msg => {
-    if (msg.content === `/ping`) {
+    if (msg.content === `/network ping`) {
       msg.reply(`Pong! The ping is **${(client.ping).toFixed(0)}**ms!  :ping_pong:`)
     }
   });
 
   client.on('message', msg => {
-    if (msg.content === `/invite`) {
-      msg.channel.sendMessage('**__Invite The Bot:__**\n \nhttps://discordapp.com/api/oauth2/authorize?client_id=406014826419191808&permissions=0&scope=bot');
+    if (msg.content === `/network invite`) {
+      msg.channel.sendMessage('**__Invite The Bot:__**\n \nhttps://discordapp.com/api/oauth2/authorize?client_id=426357120993198090&permissions=0&scope=bot');
     }
   });
 
   client.on('message', msg => {
-    if (msg.content === `/youtube`) {
-      msg.channel.sendMessage('**__Youtube Cahnnel:__**\n \nhttps://www.youtube.com/channel/UCVr5-pz35ZRK_NaCDb3NpZg');
+    if (msg.content === `/network youtube`) {
+      msg.channel.sendMessage('**__Youtube Cahnnel:__**\n \nhttps://www.youtube.com/channel/UCuPaetf28pN6knc1MdpriCg?view_as=subscriber');
     }
   });
 
   client.on('message', msg => {
-    if (msg.content === '/help') {
-      msg.channel.sendMessage(`__Derpy's House Commands:__\n \n**/discord** - Discord Link\n**/creator** - The Creators of the Bot\n**/apply** - Staff Apply\n**/test** - for know if the Bot is Online\n**/ping** - Show to you how much Ping you have\n**/invite** - Invite the Bot\n**/youtube** - Show to you the link to Youtube Channel\n**/help** - Show this Menu`);
+    if (msg.content === '/network help') {
+      msg.channel.sendMessage(`__MigServer Commands:__\n \n**/network discord** - Discord Link\n**/network creator** - The Creators of the Bot\n**/network apply** - Staff Apply\n**/network test** - for know if the Bot is Online\n**/network ping** - Show to you how much Ping you have\n**/network invite** - Invite the Bot\n**/network youtube** - Show to you the link to Youtube Channel\n**/network help** - Show this Menu`);
     }
   });
 
