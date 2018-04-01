a6f89fa45dad0fead1f21280a2a46bd8dc33b6ec
Derpy666 <37512753+Derpy666@users.noreply.github.com>
2018-04-01 12:10:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ client.on("message", (message) => {
     }});
   } else
   if (message.content === (prefix + "help")) {
-    message.channel.send({embed: {
+    message.author.sendMessage({embed: {
       color: 3447003,
       title: "__:הפקודות של בננה סרבר__\n \n",
       description: "** **\n**/ping** - מראה לך כמה פינג יש לך\n**/discord** - הקישור לדיסקורד\n**/test** - מראה לך אם הבוטית פועלת\n**/help** - מראה לך את התפריט הזה",
